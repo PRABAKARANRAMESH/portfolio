d2a3224a8abc51a8d242c51b5af8247f09df9a8a
Prabakaran <NTS0104@hepl.local>
2025-07-11 11:19:45 +0530

    Vite Config change final

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
- 
-export default defineConfig(() => {
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
- 
-  return {
-    plugins: [react()],
-   base: '/portfolio/',
-    build: {
-      chunkSizeWarningLimit: 800,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ['react', 'react-dom'],
-            mui: ['@mui/material', '@mui/icons-material'],
-            // Add more only if they're installed and used
-          },
-        },
-      },
-    },
-  };
-});
- 
- 
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
